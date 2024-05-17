7c9953dff336f82d8108b2d89e4b1da2690ba72d
cdabeykoon <135416230+cdabeykoon@users.noreply.github.com>
2024-05-17 09:25:11 +0530

    Add files via upload

---
+++ Lab sheet 5/Lab shett 05,Q3/Lab shett 05,Q3/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_shett_05_Q3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            if (validation())
+            {
+                User newUser = new User
+                    
+                    {
+                        FirstName = textBox1.Text,
+                        LastName = textBox2.Text,
+                        UserName = textBox3.Text,
+                        DateOfBirth = dateTimePicker1.Text,
+                        Email = textBox4.Text,
+                        Address = textBox5.Text,
+                        Password = textBox6.Text,
+                        ConfirmPassword = textBox7.Text,
+
+                    };
+
+                Form2 welcom = new Form2(newUser);
+                welcom.Show();
+                this.Hide();
+            }
+        }
+
+        public bool validation()
+        {
+            if(string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Enter Your First name");
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
---
+++ Lab sheet 5/Lab shett 05,Q3/Lab shett 05,Q3/Form2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_shett_05_Q3
+{
+    public partial class Form2 : Form
+    {
+        private User user;
+        public Form2(User user)
+        {
+            InitializeComponent();
+            this.user = user; 
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            label1.Text = "welcome: " + user.FirstName.ToString() + "" + user.LastName.ToString();
+            label2.Text = "Username: " + user.UserName.ToString();
+            label3.Text = "Email: " + user.Email.ToString();
+            label4.Text = "Birth Day " + user.DateOfBirth.ToString();
+
+        }
+
+    }
+}
